d1dd6f0e231a2dbc7a29da378e8de1c0dc9ae9ee
AAWA-byt <mail@aaron.is>
2022-06-26 16:19:18 +0200

    :zap: added starting log

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const {api_request} = require("./functions")
 const fs = require('fs');
 const config = JSON.parse(fs.readFileSync('config.json', 'utf8'))
 
+console.log("______________________________________")
+console.log("Application started...")
+console.log("Name: discord-weather-report")
+console.log("Author: AAWA-byt")
+console.log(new Date());
+console.log("______________________________________")
 
-var executeIntervall = setInterval(function() {     // method for repeating request
 
+const executeIntervall = setInterval(function () {
     api_request()
-
 }, config.time);  // execute time intervall
